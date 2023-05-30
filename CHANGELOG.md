41f46812a5e694c8d827873314ee1daa45163e2e
Vincent Rolea <vincent.rolea@gmail.com>
2023-05-30 14:32:33 +0200

    Bumped to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## [Unreleased]
 
+## [0.2.0] - 2023-05-30
+
 - Add dataset iteration 
 - Add test workflow 
 - Add Session string parsing
--- test/ir_telemetry_test.rb
+++ test/ir_telemetry_test.rb
@@ -5,6 +5,6 @@ require "test_helper"
 class TestIRTelemetry < Minitest::Test
   test "version number" do
     refute_nil ::IRTelemetry::VERSION
-    assert_equal "0.1.0", ::IRTelemetry::VERSION
+    assert_equal "0.2.0", ::IRTelemetry::VERSION
   end
 end
